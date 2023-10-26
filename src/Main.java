034c1a780a00cd7d8077423fe23a7d203ae072e1
admin-rez <kraken_1111@mail.ru>
2023-10-26 15:35:12 +0300

    не работает

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
-    public static void main(String[] args) {
 
-        task();
-    }
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
@@ -13,27 +10,41 @@ public class Main {
         return arr;
     }
 
-    public static void task() {
+    public static void task1() {
         int[] arr = generateRandomArray();
         double[] payments = {5000.50, 12345.00, -3434.34, 0, 560.87, -1000.00};
         double total = 0.0;
-        for (double payment : payments)
-            total += payment;
+        for (double payment : payments) ;
+        total += payment;
 
         System.out.println("Сумма выплат за месяц составляет " + total + "рублей");
+    }
+
+    public static void task2() {
+        double[] transactions = {100.23, -20.12, 56.00, 123.45, -98.76, 78.90};
+        double min = transactions[0];
+        double max = transactions[0];
+
+        for (double d : transactions) {
+            if (d < min) min = d;
+            if (d > max) max = d;
+
+        System.out.printf("Минимальная сумма трат за день составила" + min + "рублей",
+                "Максимальная сумма трат за день составила" + max + "рублей");
 
     }
-    public static void task1() {
+
+    public static void task3() {
         int[] payments = generateRandomArray();
         double averageSpend = 0;
         for (int i = 0; i < payments.length; i++) {
             averageSpend += payments[i];
             averageSpend /= payments.length;
-            System.out.println("Средняя сумма трат за месяц: " + averageSpend + " рублей");
+            System.out.println("Средняя сумма затрат за месяц: " + averageSpend + " рублей");
+
         }
 
-
-        public static void task2 () {
+        public static void task4 () {
             int[] reverseFullName = generateRandomArray();
             char[] reverseFullName1 = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
             String result = "";
